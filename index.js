dd7928c11b15a7d3e0737639d5ed696021bc7201
laylysik <58001891+laylysik@users.noreply.github.com>
2019-11-20 23:16:58 +0300

    "init"

---
+++ index.js
@@ -0,0 +1,25 @@
+
+// Подключаем библиотеку для работы с Telegram API в переменную
+var TelegramBot = require('node-telegram-bot-api');
+
+// Устанавливаем токен, который выдавал нам бот
+var token = '1029407549:AAHNiAJkrkGII6igOHTJYdpKbSGTNnVrJQ8';
+// Включить опрос сервера. Бот должен обращаться к серверу Telegram, чтобы получать актуальную информацию
+// Подробнее: https://core.telegram.org/bots/api#getupdates
+var bot = new TelegramBot(token, { polling: true });
+
+// Написать мне ... (/echo Hello World! - пришлет сообщение с этим приветствием, то есть "Hello World!")
+bot.onText(/\/echo (.+)/, function (msg, match) {
+    var fromId = msg.from.id; // Получаем ID отправителя
+    var resp = match[1]; // Получаем текст после /echo
+    bot.sendMessage(fromId, resp);
+});
+
+// Простая команда без параметров
+bot.on('message', function (msg) {
+    var chatId = msg.chat.id; // Берем ID чата (не отправителя)
+    // Фотография может быть: путь к файлу, поток (stream) или параметр file_id
+    var photo = 'cats.png'; // в папке с ботом должен быть файл "cats.png"
+    bot.sendPhoto(chatId, photo, { caption: 'Милые котята' });
+});
+
---
+++ reminder.js
@@ -0,0 +1,26 @@
+var TelegramBot = require('node-telegram-bot-api');
+
+var token = '1029407549:AAHNiAJkrkGII6igOHTJYdpKbSGTNnVrJQ8';
+var bot = new TelegramBot(token, {polling: true});
+
+var notes = [];
+
+bot.onText(/напомни (.+) в (.+)/, function (msg, match) {
+    var userId = msg.from.id;
+    var text = match[1];
+    var time = match[2];
+
+    notes.push({ 'uid': userId, 'time': time, 'text': text });
+
+    bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
+});
+
+setInterval(function(){
+    for (var i = 0; i < notes.length; i++) {
+    const curDate = new Date().getHours() + ':' + new Date().getMinutes();
+    if (notes[i]['time'] === curDate) {
+      bot.sendMessage(notes[i]['uid'], 'Напоминаю, что вы должны: '+ notes[i]['text'] + ' сейчас.');
+      notes.splice(i, 1);
+    }
+  }
+}, 1000);
