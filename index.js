9b0b04702c4b2a7fa5a98bbe341e05be44d0ef6e
laylysik <58001891+laylysik@users.noreply.github.com>
2019-11-21 00:48:41 +0300

    "init"

--- index.js
+++ index.js
@@ -1,33 +1,8 @@
-
-// Подключаем библиотеку для работы с Telegram API в переменную
-var TelegramBot = require('node-telegram-bot-api');
-
-// Устанавливаем токен, который выдавал нам бот
-var token = '1029407549:AAHNiAJkrkGII6igOHTJYdpKbSGTNnVrJQ8';
-// Включить опрос сервера. Бот должен обращаться к серверу Telegram, чтобы получать актуальную информацию
-// Подробнее: https://core.telegram.org/bots/api#getupdates
-var bot = new TelegramBot(token, { polling: true });
-
-// Написать мне ... (/echo Hello World! - пришлет сообщение с этим приветствием, то есть "Hello World!")
-bot.onText(/\/echo (.+)/, function (msg, match) {
-    var fromId = msg.from.id; // Получаем ID отправителя
-    var resp = match[1]; // Получаем текст после /echo
-    bot.sendMessage(fromId, resp);
-});
-
-// Простая команда без параметров
-bot.on('message', function (msg) {
-    var chatId = msg.chat.id; // Берем ID чата (не отправителя)
-    // Фотография может быть: путь к файлу, поток (stream) или параметр file_id
-    var photo = 'cats.png'; // в папке с ботом должен быть файл "cats.png"
-    bot.sendPhoto(chatId, photo, { caption: 'Милые котята' });
-});
-
 const Telegraf = require('telegraf')
 
 const bot = new Telegraf(process.env.1029407549:AAHNiAJkrkGII6igOHTJYdpKbSGTNnVrJQ8)
-bot.start((ctx) => ctx.reply('Привет! Как твои дела?'))
-bot.help((ctx) => ctx.reply('Чем я могу тебе помочь?'))
+bot.start((ctx) => ctx.reply('Welcome!'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply(''))
-bot.hears('Привет', (ctx) => ctx.reply('И тебе привет :)'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 bot.launch()
